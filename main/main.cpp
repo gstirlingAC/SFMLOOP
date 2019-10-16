0ea90b5873452e2e2b91839e5a2ae7bddaa669a8
Gordon Stirling <gordon.stirling@ayrshire.ac.uk>
2019-10-16 16:13:27 +0100

    Tutorial 1 - Create an SFML window By the end of this tutorial you will be able to create a simple window, apply a window colour, title bar etc.

--- main/main.cpp
+++ main/main.cpp
@@ -1,21 +1,24 @@
 // main.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "pch.h"
 #include <iostream>
+#include "SFML/x86/include/SFML/Graphics.hpp"
+
+sf::Vector2f viewSize(1024, 768);
+sf::VideoMode vm(viewSize.x, viewSize.y);
+sf::RenderWindow window(vm, "Hello SFML Game!!!", sf::Style::Default);
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	// Initialize Game Objects
+	while (window.isOpen()) 
+	{
+		// Handle Keyboard Events
+		// Update Game Objects in the scene
+		window.clear(sf::Color::Red);
+		// Render game Objects
+		window.display();
+	}
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
