58605177d8119df8799d56c966c31f617afff27e
Gordon Stirling <gordon.stirling@ayrshire.ac.uk>
2019-10-16 20:27:47 +0100

    Tutorial 3 - Add your first graphic in SFML By the end of this tutorial you should be able to load a graphic in to memory, and display it within the window.

--- main/main.cpp
+++ main/main.cpp
@@ -9,41 +9,41 @@ sf::Vector2f viewSize(1024, 768);
 sf::VideoMode vm(viewSize.x, viewSize.y);
 sf::RenderWindow window(vm, "Hello SFML Game!!!", sf::Style::Default);
 
+// Set up graphics
+sf::Texture skyTexture;
+sf::Sprite skySprite;
+
+void init();
+void draw();
+
 int main()
 {
 	// Initialize Game Objects
-	// Set up a rectangle shape
-	sf::RectangleShape rect(sf::Vector2f(500.0f, 300.0f));
-	rect.setFillColor(sf::Color::Yellow);
-	rect.setPosition(viewSize.x / 2, viewSize.y / 2);
-	rect.setOrigin(sf::Vector2f(rect.getSize().x / 2, rect.getSize().y / 2));
+	init();
 
-	// Set up a circle shape
-	sf::CircleShape circle(100);
-	circle.setFillColor(sf::Color::Green);
-	circle.setPosition(viewSize.x / 2, viewSize.y / 2);
-	circle.setOrigin(sf::Vector2f(circle.getRadius(), circle.getRadius()));
-
-	// Set up a triangle shape
-	sf::ConvexShape triangle;
-	triangle.setPointCount(3);
-	triangle.setPoint(0, sf::Vector2f(-100, 0));
-	triangle.setPoint(1, sf::Vector2f(0, -100));
-	triangle.setPoint(2, sf::Vector2f(100, 0));
-	triangle.setFillColor(sf::Color(128, 0, 128, 255));
-	triangle.setPosition(viewSize.x / 2, viewSize.y / 2);
-
+	// Game loop
 	while (window.isOpen()) 
 	{
 		// Handle Keyboard Events
 		// Update Game Objects in the scene
 		window.clear(sf::Color::Red);
 		// Render game Objects
-		window.draw(rect);
-		window.draw(circle);
-		window.draw(triangle);
+		draw();
 		window.display();
 	}
 	return 0;
 }
 
+void init()
+{
+	// Load sky texture
+	skyTexture.loadFromFile("Assets/graphics/sky.png");
+
+	// Set and attach a texture to sprite
+	skySprite.setTexture(skyTexture);
+}
+
+void draw()
+{
+	window.draw(skySprite);
+}
